58f2993e5da8249ec0d1254d2b1dd4f7c4de99bd
Enno Runne <458526+ennru@users.noreply.github.com>
2020-12-17 09:31:53 +0100

    Debug secret conversion

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -5,6 +5,7 @@ if [ -z ${SCP_SECRET} ]; then
 fi
 make build-wip
 eval "$(ssh-agent -s)"
-echo ${SCP_SECRET} | base64 -d > /tmp/id_rsa
+printf "%.10s\n" ${SCP_SECRET}
+echo ${SCP_SECRET} | base64 -d --ignore-garbage > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
 scp -i /tmp/id_rsa -o UserKnownHostsFile=.travis/known_hosts -r target/* akkarepo@gustav.akka.io:akka.io/platform-guide/wip/
